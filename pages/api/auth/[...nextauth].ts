ca511c0591e5f4008af929e528b0cc7ba5985727
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-03-12 23:18:18 +0530

    perf: :fire: Login Performance Increased

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import { SIGN_IN } from "../../../graphql/mutations"
-import { client } from "../../../graphql/configure"
+// import { SIGN_IN } from "../../../graphql/mutations"
+// import { client } from "../../../graphql/configure"
 import { createUserName } from "../../../hooks/createUserName"
+import { registerUser } from "../../../helpers/registerUser"
 
 export default NextAuth({
   providers: [
@@ -28,48 +29,69 @@ export default NextAuth({
       if (provider === 'google'){
         let username = createUserName(profile.email)
 
-        const {data, errors} = await client.mutate({
-          mutation: SIGN_IN,
-          variables: {
-            payload : {
-              name: profile.name,
-              username: username,
-              email: profile.email,
-              providerId: profile.sub,
-              image: profile.picture,
-            }
-          }
-        })
+        // const {data, errors} = await client.mutate({
+        //   mutation: SIGN_IN,
+        //   variables: {
+        //     payload : {
+        //       name: profile.name,
+        //       username: username,
+        //       email: profile.email,
+        //       providerId: profile.sub,
+        //       image: profile.picture,
+        //     }
+        //   }
+        // })
 
-        if(errors) {
-          console.log(errors)
-          return false
-        }
+        // if(errors) {
+        //   console.log(errors)
+        //   return false
+        // }
 
-        return data.signIn.success
+        // return data.signIn.success
+
+        const isVerified = await registerUser(
+          profile.name,
+          username,
+          profile.email,
+          profile.sub,
+          profile.picture.toString()
+        )
+
+        return isVerified;
       }
 
       else if(provider === 'github'){
-        const {data, errors} = await client.mutate({
-          mutation: SIGN_IN,
-          variables: {
-            payload: {
-              name: profile.name,
-              username: profile.login,
-              email: profile.email,
-              providerId: profile.id,
-              image: profile.avatar_url,
-              githubProfile: profile.html_url
-            }
-          }
-        })
+        // const {data, errors} = await client.mutate({
+        //   mutation: SIGN_IN,
+        //   variables: {
+        //     payload: {
+        //       name: profile.name,
+        //       username: profile.login,
+        //       email: profile.email,
+        //       providerId: profile.id,
+        //       image: profile.avatar_url,
+        //       githubProfile: profile.html_url
+        //     }
+        //   }
+        // })
 
-        if(errors){
-          console.log(errors)
-          return false
-        }
+        // if(errors){
+        //   console.log(errors)
+        //   return false
+        // }
 
-        return data.signIn.success
+        // return data.signIn.success
+
+        const isVerified = await registerUser(
+          profile.name,
+          (profile.login).toString(),
+          profile.email,
+          (profile.id).toString(),
+          (profile.avatar_url).toString(),
+          (profile.html_url).toString()
+        )
+
+        return isVerified;
       }
     },
     async session({ session, token }) {
